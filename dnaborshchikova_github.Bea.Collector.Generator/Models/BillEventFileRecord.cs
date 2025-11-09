82f42cf360b5281040fc83ff1ab480b02667d556
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2025-11-09 19:57:39 +0300

    #1 Реализация структуры проекта

---
+++ dnaborshchikova_github.Bea.Collector.Generator/Models/BillEventFileRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnaborshchikova_github.Bea.Collector.Generator.Models
+{
+    public class BillEventFileRecord
+    {
+        public Guid Id { get; set; }
+        public DateTime OperationDateTime { get; set; }
+        public Guid UserId { get; set; }
+        public string EventType { get; set; } // "bill_payed" или "bill_canceled"
+        public decimal Amount { get; set; }
+        public string Number { get; set; }
+
+        public Guid? BuyerId { get; set; }
+        public decimal? CancelAmount { get; set; }
+    }
+}
---
+++ dnaborshchikova_github.Bea.Collector/Handlers/BillEventFileDatabaseSender.cs
@@ -0,0 +1,13 @@
+﻿using dnaborshchikova_github.Bea.Collector.CollertorService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnaborshchikova_github.Bea.Collector.CollertorService.Handlers
+{
+    public class BillEventFileDatabaseSender : IEventSender
+    {
+    }
+}
