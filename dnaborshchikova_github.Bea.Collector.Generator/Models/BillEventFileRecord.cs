ba7f64773c497476d69bf28fcfdd7072d992af04
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2025-11-10 22:52:41 +0300

    Добвлен класс GenerateDataService

--- dnaborshchikova_github.Bea.Collector.Generator/Models/BillEventFileRecord.cs
+++ dnaborshchikova_github.Bea.Collector.Generator/Models/BillEventFileRecord.cs
@@ -11,7 +11,7 @@ namespace dnaborshchikova_github.Bea.Collector.Generator.Models
         public Guid Id { get; set; }
         public DateTime OperationDateTime { get; set; }
         public Guid UserId { get; set; }
-        public string EventType { get; set; } // "bill_payed" или "bill_canceled"
+        public string EventType { get; set; }
         public decimal Amount { get; set; }
         public string Number { get; set; }
 
