7b4a694f8eb5fc56081c1b82d62f67490249b006
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2025-11-18 22:12:45 +0300

    #3 Реализация генератора

---
+++ dnaborshchikova_github.Bea.Collector.Generator/Services/BillEventService.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using dnaborshchikova_github.Bea.Collector.Generator.Models;
+
+namespace dnaborshchikova_github.Bea.Generator.Services
+{
+    public static class BillEventService
+    {
+        public static List<BillEventFileRecord> GetBillEvents(int paidBillEventRecordCount
+            , int cancelledBillEventRecordCount)
+        {
+            var date = DateTime.Today;
+
+            var payedFaker = new Faker<BillEventFileRecord>("ru")
+                .RuleFor(e => e.OperationDateTime, f => f.Date.Between(date.AddHours(0), date.AddHours(23.999)))
+                .RuleFor(e => e.UserId, f => f.Random.Guid())
+                .RuleFor(e => e.EventType, "bill_payed")
+                .RuleFor(e => e.Amount, f => Math.Round(f.Finance.Amount(10, 5000), 2))
+                .RuleFor(e => e.CancelAmount, (f, e) => null)
+                .RuleFor(e => e.BillNumber, (f, e) => (100000 + f.IndexFaker).ToString())
+                .RuleFor(e => e.Id, f => f.Random.Guid())
+                .RuleFor(e => e.BuyerId, f => f.Random.Bool(0.5f) ? f.Random.Guid() : (Guid?)null);
+
+            var canceledFaker = new Faker<BillEventFileRecord>("ru")
+                .RuleFor(e => e.OperationDateTime, f => f.Date.Between(date.AddHours(0), date.AddHours(23.999)))
+                .RuleFor(e => e.UserId, f => f.Random.Guid())
+                .RuleFor(e => e.EventType, "bill_canceled")
+                .RuleFor(e => e.Amount, f => Math.Round(f.Finance.Amount(10, 5000), 2))
+                .RuleFor(e => e.CancelAmount, (f, e) => Math.Round(f.Finance.Amount(1, e.Amount), 2))
+                .RuleFor(e => e.BillNumber, (f, e) => (100000 + f.IndexFaker).ToString())
+                .RuleFor(e => e.Id, f => f.Random.Guid())
+                .RuleFor(e => e.BuyerId, (f, e) => null);
+
+            var payed = payedFaker.Generate(paidBillEventRecordCount);
+            var canceled = canceledFaker.Generate(cancelledBillEventRecordCount);
+            var billEvents = payed.Concat(canceled).OrderBy(e => e.OperationDateTime).ToList();
+
+            return billEvents;
+        }
+    }
+}
