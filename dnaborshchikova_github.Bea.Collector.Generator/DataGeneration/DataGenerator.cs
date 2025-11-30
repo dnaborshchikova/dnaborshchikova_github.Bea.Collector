e003deed8cdcf7283b513fb81251d0542470b391
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2025-11-30 19:31:47 +0300

    #3 поправлена генерация файла

--- dnaborshchikova_github.Bea.Collector.Generator/DataGeneration/DataGenerator.cs
+++ dnaborshchikova_github.Bea.Collector.Generator/DataGeneration/DataGenerator.cs
@@ -14,7 +14,7 @@ namespace dnaborshchikova_github.Bea.Generator.DataGeneration
                 .RuleFor(x => x.EventType, f => "bill_payed")
                 .RuleFor(x => x.Amount, f => f.Random.Decimal(100, 5000))
                 .RuleFor(x => x.Number, f => f.Random.Int(100000, 999999).ToString())
-                .RuleFor(x => x.BuyerId, f => f.Random.Bool(0.6f) ? Guid.NewGuid() : (Guid?)null);
+                .RuleFor(x => x.BuyerId, f => Guid.NewGuid());
 
             var cancelledBillFaker = new Faker<CancelledBillEvent>("ru")
                 .RuleFor(x => x.Id, f => Guid.NewGuid())
--- dnaborshchikova_github.Bea.Collector.Generator/FileGeneration/CsvFileGenerator.cs
+++ dnaborshchikova_github.Bea.Collector.Generator/FileGeneration/CsvFileGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using dnaborshchikova_github.Bea.Collector.Core.Models;
+using System.Globalization;
 using System.Reflection;
 using System.Text;
 
@@ -13,15 +14,24 @@ namespace dnaborshchikova_github.Bea.Generator.FileGeneration
 
             using (var writer = new StreamWriter(filePath, false, Encoding.UTF8))
             {
-                writer.WriteLine("Id,UserId,BuyerId,BillNumber,EventType,OperationDateTime,Amount,CancelAmount");
+                var billEventProperties = typeof(BillEvent).GetProperties();
+                var paidBillEventProperties = typeof(PaidBillEvent).GetProperties();
+                var cancelledBillEventProperties = typeof(CancelledBillEvent).GetProperties();
+                var allProperties = billEventProperties
+                    .Concat(paidBillEventProperties)
+                    .Concat(cancelledBillEventProperties)
+                    .Select(p => p.Name)
+                    .Distinct();
+
+                writer.WriteLine(string.Join(",", allProperties));
+
                 foreach (var bill in billEvents)
                 {
-
                     var buyerId = bill is PaidBillEvent p ? p.BuyerId?.ToString() ?? "" : "";
-                    var cancelAmount = bill is CancelledBillEvent c ? c.CancelAmount?.ToString() ?? "" : "";
+                    var cancelAmount = bill is CancelledBillEvent c ? c.CancelAmount?.ToString(CultureInfo.InvariantCulture) ?? "" : "";
 
-                    writer.WriteLine($"{bill.Id},{bill.UserId},{buyerId},{bill.Number},{bill.EventType}" +
-                        $",{bill.OperationDateTime:O},{bill.Amount},{cancelAmount}");
+                    writer.WriteLine($"{bill.Id},{bill.OperationDateTime},{bill.UserId},{bill.EventType}" +
+                        $",{bill.Amount.ToString(CultureInfo.InvariantCulture)},{bill.Number},{buyerId},{cancelAmount}");
                 }
             }
         }
