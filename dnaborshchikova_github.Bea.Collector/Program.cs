f8f9b5df316335872e7aa874b814b6bc5de27117
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2025-12-03 20:50:04 +0300

    #3 В конфиг добавлены параметры FilePath, GenerateFile

--- dnaborshchikova_github.Bea.Collector/Program.cs
+++ dnaborshchikova_github.Bea.Collector/Program.cs
@@ -6,8 +6,20 @@ var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
+if (!bool.TryParse(config["GenerateFile"], out var isGenerateFile))
+    throw new Exception($"Конфигурационный файл не настроен. Не задан параметр isGenerateFile.");
+
+if (!isGenerateFile)
+{
+    var filePath = config["FilePath"];
+    if (string.IsNullOrWhiteSpace(filePath))
+        throw new ArgumentException("Конфигурационный файл не настроен." +
+            "Не указан путь к файлу. Укажите путь или поставьте значение false для параметра GenerateFile.");
+    return;
+}
+
 if (!bool.TryParse(config["RunAsProcess"], out var isRunAsProcess))
-    throw new Exception($"Конфигурационный файл не настроен.\nНе задан параметр RunAsProcess.");
+    throw new Exception($"Конфигурационный файл не настроен. Не задан параметр RunAsProcess.");
 
 if (isRunAsProcess)
 {
@@ -16,7 +28,6 @@ if (isRunAsProcess)
     proccess.Start();
     return;
 }
-
 
 var errors = new List<string>();
 if (!int.TryParse(config["PaidBillEventCount"], out var paidBillEventCount))
