ac94148d5faf224339cfacddaf7ca73f6aa758b1
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2025-12-14 15:51:44 +0300

    #9 Валидация вынесена в отдельный класс, настройки передаются через DI

---
+++ dnaborshchikova_github.Bea.Collector/AppSettingsService.cs
@@ -0,0 +1,86 @@
+﻿using dnaborshchikova_github.Bea.Collector.Core.Models;
+using dnaborshchikova_github.Bea.Generator;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnaborshchikova_github.Bea.Collector.App
+{
+    public class AppSettingsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppSettingsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public AppSettings CreateAppSettings()
+        {
+            var errors = new List<string>();
+
+            var isGenerateFile = false;
+            if (!bool.TryParse(_configuration["GenerateFile"], out isGenerateFile))
+                errors.Add($"Не задан параметр isGenerateFile.");
+            
+            var threadCount = 0;
+            if (!int.TryParse(_configuration["ThreadCount"], out threadCount))
+                errors.Add($"Не задан параметр ThreadCount.");
+
+            var processType = _configuration["ProcessType"];
+            if (string.IsNullOrEmpty(processType))
+                errors.Add("Не указаан способ обрабоки.");
+
+            if (errors.Count > 0)
+            {
+                throw new Exception($"Конфигурационный файл не настроен." +
+                    $"Список ошибок:\n{string.Concat("\n", errors)}\n");
+            }
+
+            var filePath = string.Empty;
+            var fileFormat = string.Empty;
+            var isRunAsProcess = false;
+            var paidBillEventCount = 0;
+            var cancelledBillEventCount = 0;
+
+            if (!isGenerateFile)
+            {
+                filePath = _configuration["FilePath"];
+                if (string.IsNullOrWhiteSpace(filePath))
+                    throw new ArgumentException("Конфигурационный файл не настроен." +
+                        "Не указан путь к файлу. Укажите путь или поставьте значение false для параметра GenerateFile.");
+            }
+            else
+            {
+                if (!bool.TryParse(_configuration["RunAsProcess"], out isRunAsProcess))
+                    throw new Exception($"Конфигурационный файл не настроен. Не задан параметр RunAsProcess.");
+
+                if (!isRunAsProcess)
+                {
+                    errors = new List<string>();
+                    if (!int.TryParse(_configuration["PaidBillEventCount"], out paidBillEventCount))
+                        errors.Add("PaidBillEventCount");
+
+                    if (!int.TryParse(_configuration["CancelledBillEventCount"], out cancelledBillEventCount))
+                        errors.Add("CancelledBillEventCount");
+
+                    fileFormat = _configuration["FileFormat"];
+                    if (string.IsNullOrWhiteSpace(fileFormat))
+                        errors.Add("FileFormat");
+
+                    if (errors.Count > 0)
+                        throw new Exception($"Конфигурационный файл не настроен.\nНе заданы параметры:\n {string.Join("\n", errors)}");
+
+                }
+            }
+
+            var appsettings = new AppSettings(isRunAsProcess, isGenerateFile, filePath, fileFormat,
+                paidBillEventCount, cancelledBillEventCount, threadCount, processType);
+            return appsettings;
+        }
+    }
+}
--- dnaborshchikova_github.Bea.Collector/Program.cs
+++ dnaborshchikova_github.Bea.Collector/Program.cs
@@ -1,4 +1,5 @@
-﻿using dnaborshchikova_github.Bea.Collector.Core.Interfaces;
+﻿using dnaborshchikova_github.Bea.Collector.App;
+using dnaborshchikova_github.Bea.Collector.Core.Interfaces;
 using dnaborshchikova_github.Bea.Collector.Core.Models;
 using dnaborshchikova_github.Bea.Collector.Parser.Handlers;
 using dnaborshchikova_github.Bea.Collector.Processor.Handlers;
@@ -11,88 +12,30 @@ using Microsoft.Extensions.Hosting;
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
+var validator = new AppSettingsService(config);
+var appSettings = validator.CreateAppSettings();
 
-var isRunAsProcess = false;
-var isGenerateFile = false;
-var filePath = string.Empty;
-var fileFormat = string.Empty;
-var paidBillEventCount = 0;
-var cancelledBillEventCount = 0;
-var threadCount = 0;
-var processType = string.Empty;
-var appsettings = new AppSettings();
+#region Запуск генератора
 
-#region Валидация запуска
-
-var errors = new List<string>();
-
-if (!bool.TryParse(config["GenerateFile"], out isGenerateFile))
-    errors.Add($"Не задан параметр isGenerateFile.");
-
-if (!int.TryParse(config["ThreadCount"], out threadCount))
-    errors.Add($"Не задан параметр ThreadCount.");
-
-processType = config["ProcessType"];
-if (string.IsNullOrEmpty(processType))
-    errors.Add("Не указан путь к файлу. Укажите путь или поставьте значение false для параметра ProcessType.");
-
-if (errors.Count > 0)
+if (appSettings.RunAsProcess && appSettings.GenerateFile)
 {
-    throw new Exception($"Конфигурационный файл не настроен." +
-        $"Список ошибок:\nНе задан параметры {string.Concat("\n", errors)}\n");
+    var proccess = new System.Diagnostics.Process();
+    appSettings.FileFormat = Path.Combine(AppContext.BaseDirectory, $"{DateTime.Now.ToShortDateString()}_BillEvent.csv");
+    proccess.StartInfo.FileName = Path.Combine(AppContext.BaseDirectory, $"dnaborshchikova_github.Bea.Generator.exe");
+    proccess.Start();
+    proccess.WaitForExit();
+}
+else if (appSettings.GenerateFile)
+{
+    var runner = new AppRunner(appSettings);
+    runner.Generate();
 }
 
-if (!isGenerateFile)
-{
-    filePath = config["FilePath"];
-    if (string.IsNullOrWhiteSpace(filePath))
-        throw new ArgumentException("Конфигурационный файл не настроен." +
-            "Не указан путь к файлу. Укажите путь или поставьте значение false для параметра GenerateFile.");
-}
-else
-{
-    if (!bool.TryParse(config["RunAsProcess"], out isRunAsProcess))
-        throw new Exception($"Конфигурационный файл не настроен. Не задан параметр RunAsProcess.");
-
-    if (isRunAsProcess)
-    {
-        var proccess = new System.Diagnostics.Process();
-        filePath = Path.Combine(AppContext.BaseDirectory, $"{DateTime.Now.ToShortDateString()}_BillEvent.csv");
-        proccess.StartInfo.FileName = Path.Combine(AppContext.BaseDirectory, $"dnaborshchikova_github.Bea.Generator.exe");
-        proccess.Start();
-        proccess.WaitForExit();
-    }
-    else
-    {
-        errors = new List<string>();
-        if (!int.TryParse(config["PaidBillEventCount"], out paidBillEventCount))
-            errors.Add("PaidBillEventCount");
-
-        if (!int.TryParse(config["CancelledBillEventCount"], out cancelledBillEventCount))
-            errors.Add("CancelledBillEventCount");
-
-        if (string.IsNullOrWhiteSpace(fileFormat))
-            errors.Add("FileFormat");
-
-        if (errors.Count > 0)
-            throw new Exception($"Конфигурационный файл не настроен.\nНе заданы параметры:\n {string.Join("\n", errors)}");
-
-        appsettings = new AppSettings(false, false, string.Empty, fileFormat,
-            paidBillEventCount, cancelledBillEventCount, 0, string.Empty);
-
-
-        var runner = new AppRunner(appsettings);
-        runner.Generate();
-    }
-}
 #endregion
-
- appsettings = new AppSettings(isRunAsProcess, isGenerateFile, filePath, fileFormat,
-    paidBillEventCount, cancelledBillEventCount, threadCount, processType);
 
 var host = Host.CreateDefaultBuilder().ConfigureServices(services =>
     {
-        services.AddSingleton(appsettings);
+        services.AddSingleton(appSettings);
         services.AddScoped<ThreadProcessor>();
         services.AddScoped<TaskProcessor>();
         services.AddScoped<IParcer, CsvParser>();
