7fd06622c317568c49228b0e0ebe32cfabfa6da2
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2026-02-19 09:31:29 +0300

    #7 вызов генератора удален из workerservice

--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -10,11 +10,9 @@ using dnaborshchikova_github.Bea.Collector.Processor.Processors;
 using dnaborshchikova_github.Bea.Collector.Processor.Services;
 using dnaborshchikova_github.Bea.Collector.Sender.Handlers;
 using dnaborshchikova_github.Bea.Collector.WorkerService.Models;
-using dnaborshchikova_github.Bea.Generator;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Filters;
-using System.Diagnostics;
 using WorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -25,10 +23,6 @@ var processingSettings = config.GetSection(nameof(ProcessingSettings)).Get<Proce
 var workerServiceSettings = config.GetSection(nameof(WorkerServiceSettings)).Get<WorkerServiceSettings>();
 var appSettingsService = new AppSettingsService();
 var appSettings = appSettingsService.CreateAppSettings(generatorSettings, processingSettings);
-
-#region Запуск генератора
-//RunGenerator();
-#endregion
 
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(config)
@@ -67,40 +61,3 @@ builder.Services.AddDbContext<CollectorDbContext>(options =>
 });
 var host = builder.Build();
 host.Run();
-
-#region Код запуска генератора
-//void RunGenerator()
-//{
-//    if (appSettings.ProcessingSettings.GeneratorRunAsProcess && appSettings.ProcessingSettings.GenerateFile)
-//    {
-//        var proccess = new Process()
-//        {
-//            StartInfo = new ProcessStartInfo
-//            {
-//                FileName = Path.Combine(AppContext.BaseDirectory, "dnaborshchikova_github.Bea.Generator.exe"),
-//                WorkingDirectory = Path.Combine(AppContext.BaseDirectory),
-//                RedirectStandardOutput = true,
-//                RedirectStandardError = true,
-//                UseShellExecute = false,
-//                CreateNoWindow = true
-//            }
-//        };
-//        proccess.Start();
-//        var output = proccess.StandardOutput.ReadToEnd();
-//        var error = proccess.StandardError.ReadToEnd();
-//        proccess.WaitForExit();
-
-//        if (proccess.ExitCode != 0)
-//        {
-//            throw new Exception($"Generator failed: {error}");
-//        }
-
-//        appSettings.ProcessingSettings.FilePath = output.Trim();
-//    }
-//    else if (appSettings.ProcessingSettings.GenerateFile)
-//    {
-//        var runner = new AppRunner(appSettings.GeneratorSettings);
-//        appSettings.ProcessingSettings.FilePath = runner.Generate();
-//    }
-//}
-#endregion
